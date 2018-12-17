5b63ea1ff60b621bc156ba1cb54e8e9c5d6687e4
tjsnider <theodoresnider@gmail.com>
2018-12-17 13:40:43 -0500

    Added incorrect answer property, calculation, and reporting element.

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,33 +2,46 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component {
-    constructor(props) { super(props) }
+    constructor(props) { 
+        super(props) 
+        this.state = {
+            incorrectAnswer: false
+        }
+    }
     
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
             this.props.showNextQuestionHandler()
+            this.setState((state) => {
+                return { incorrectAnswer: false }
+            })
+        } else {
+            this.setState((state) => {
+                return { incorrectAnswer: true }
+            })
         }
     }
     
     render() {
         return (
             <main>
-            <section>
-                <p>{ this.props.quiz_question.instruction_text }</p>
-            </section>
-            <section className="buttons">
-                <ul>
-                    {
-                        this.props.quiz_question.answer_options.map((answer_option, index) => {
-                            return (<QuizQuestionButton 
-                                        clickHandler={ this.handleClick.bind(this) } 
-                                        key={ index } 
-                                        button_text={ answer_option } />)
-                        })
-                    }
-                </ul>
-            </section>
-        </main>
+                { this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right.</p> : null }
+                <section>
+                    <p>{ this.props.quiz_question.instruction_text }</p>
+                </section>
+                <section className="buttons">
+                    <ul>
+                        {
+                            this.props.quiz_question.answer_options.map((answer_option, index) => {
+                                return (<QuizQuestionButton 
+                                            clickHandler={ this.handleClick.bind(this) } 
+                                            key={ index } 
+                                            button_text={ answer_option } />)
+                            })
+                        }
+                    </ul>
+                </section>
+            </main>
         )
     }
 }
